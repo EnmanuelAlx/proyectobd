89f3192dbe5c8993a286d94d9a00fdc2bc77a510
EnmanuelAlx <EnmanuelAlx@gmail.com>
2018-07-07 16:43:47 -0400

    Renombrando migraciones y arreglando user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,6 +18,8 @@ $factory->define(App\User::class, function (Faker $faker) {
         'id' => 1,
         'tipo' => 0,
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+        'direccion' => '',
+        'nombre' => 'admin',
         'remember_token' => str_random(10),
     ];
 });
